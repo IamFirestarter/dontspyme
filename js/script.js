40892cb6b878216f212d7c81eec74c02b14fc99c
zutrinken <xoxo@peteramende.de>
2013-07-21 16:00:06 +0200

    border shit again

--- js/script.js
+++ js/script.js
@@ -5,16 +5,33 @@ $(document).ready(function(){
 		}, 500);
 		return false;
 	});
-	
 	function changeBorder() {
 		var text = '';
 		//var possible = '01 ';
 		var possible = 'AB CD EF 01 23 45 67 89 ';
 
-		for(var i = 0; i < 1500; i++) {
+		for(var i = 0; i < 8000; i++) {
 			text += possible.charAt(Math.floor(Math.random() * possible.length));
 		}
 		$('.content-border').text(text);
 	}
 	setInterval(changeBorder, 50);
+/* 	$(window).scroll(changeBorder) */
+	
+/*
+	var movementStrength = 50;
+	var height = movementStrength / $(window).height();
+	var width = movementStrength / $(window).width();
+	
+	$("html").mousemove(function(e){
+	
+		var pageX = e.pageX - ($(window).width() / 2);
+		var pageY = e.pageY - ($(window).height() / 2);
+		
+		var newvalueX = width * pageX * -1;
+		var newvalueY = height * pageY * -1;
+		
+		$('.content-border').css("background-position", newvalueX+"px "+newvalueY+"px");
+	});
+*/
 });
