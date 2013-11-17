ffcf9e31b9b29fd122a8628bbe5ece19835ee1df
zutrinken <xoxo@peteramende.de>
2013-11-17 01:23:15 +0100

    dynamic font and layoutsize

--- js/script.js
+++ js/script.js
@@ -1,10 +1,45 @@
 $(document).ready(function(){
+
+	/* ==========================================================================
+	   Exponential Widths & Fonts
+	   ========================================================================== */
+	   
+		function exponential() {
+			var view = $(window).width();
+			var shot = Math.sqrt(view);
+			var type = shot / 10 - 0.5;
+			shot = 100 - shot * view / 1000;
+			if(shot < 62.51) {
+				shot = 62.50;
+			} else if (shot > 89.999) {
+				shot = 90;
+			}
+			$('.inner').css('width', shot + '%');
+			if(type < 1.41) {
+				type = 1.4;
+			} else if (type > 1.999) {
+				type = 2;
+			}
+			$('body').css('font-size', type + 'em');
+		}
+		exponential();
+		$(window).resize(exponential);
+
+	/* ==========================================================================
+	   Scroll to Top
+	   ========================================================================== */
+
 	$('a').click(function(){
 		$('html, body').animate({
 			scrollTop: $( $.attr(this, 'href') ).offset().top
 		}, 500);
 		return false;
 	});
+	
+	/* ==========================================================================
+	   Border Number rotation
+	   ========================================================================== */
+	
 	function changeBorder() {
 		var text = '';
 		//var possible = '01 ';
